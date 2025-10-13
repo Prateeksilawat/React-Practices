e374db13ded334d96fd6572603fa20027370cd40
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2025-10-13 19:38:07 +0530

    add app.jsx

--- apps/react-faq-component/src/App.jsx
+++ apps/react-faq-component/src/App.jsx
@@ -1,15 +1,13 @@
-
 import './App.css';
 import Faq from './components/Faq';
 
 function App() {
-
-
   return (
-    <div className=' mt-10'>
-     
-      <h1 className='text-7xl text-red-500 font-extrabold text-center'>FAQ Component</h1>
-      <Faq/>
+    <div className=" mt-10">
+      <h1 className="text-7xl text-red-500 font-extrabold text-center">
+        FAQ Component
+      </h1>
+      <Faq />
     </div>
   );
 }
