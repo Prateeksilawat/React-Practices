0c251127944b69a628db9955155ff9cefa05db0a
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2025-10-24 22:58:47 +0530

    add new project

--- apps/react-modal-overlay/src/App.jsx
+++ apps/react-modal-overlay/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1 className="text-red-500 text-center font-bold text-5xl ">
-       Modal OverLay
+        Modal OverLay
       </h1>
 
       <p className="text-center">
