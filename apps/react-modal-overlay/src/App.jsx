c3efee1105a4d46b34529a6149df16a9c974cc10
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2025-10-24 22:57:32 +0530

    add new modal project

---
+++ apps/react-modal-overlay/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-center font-bold text-5xl ">
+       Modal OverLay
+      </h1>
+
+      <p className="text-center">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
